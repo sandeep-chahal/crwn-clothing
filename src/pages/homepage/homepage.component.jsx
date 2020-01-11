77c077418fb8dbdec3e28f6d6f697d1d8890559f
sandeep chahal <sandeepchahal573@gmail.com>
2020-01-11 18:34:19 +0530

    closed

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import './homepage.styles.scss';
-import Directory from '../../components/directory/directory.component';
+import React from "react";
+// import './homepage.styles.scss';
+import Directory from "../../components/directory/directory.component";
+import { HomepageContainer } from "./homepage.styled.component";
 
 const HomePage = () => (
-    <div  className="homepage" >
-        <Directory />
-    </div>
-)
+  <HomepageContainer>
+    <Directory />
+  </HomepageContainer>
+);
 
 export default HomePage;
